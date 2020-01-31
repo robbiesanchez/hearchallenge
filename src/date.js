ba637e7d1642b3d2f41f0ae88150079ddcc93731
robbiesanchez <rfsanchez004@gmail.com>
2020-01-30 22:30:07 -0500

    spanish version, mobile friendly, form, responsive updates

--- src/date.js
+++ src/date.js
@@ -1,7 +1,11 @@
-var d = new Date();
-var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+let d = new Date();
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 console.log(months)
-var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 d.setDate(-5);
 
 document.getElementById("date").innerHTML = (days[d.getDay()]+", "+months[d.getMonth()]+ " " + 26 + ", " + d.getFullYear())
+
+let theHeader = "Hear Me"
+
+document.getElementsByTagName("h1").innerHTML = theHeader
