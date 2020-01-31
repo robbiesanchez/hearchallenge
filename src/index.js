f57ee912540375a45ba0d5af5dc640dca760999d
robbiesanchez <rfsanchez004@gmail.com>
2020-01-31 10:34:05 -0500

    added comments

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// Dynamic Date 
 let d = new Date();
 let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
 console.log(months)
@@ -6,10 +7,13 @@ d.setDate(-5);
 
 document.getElementById("date").innerHTML = (days[d.getDay()]+", "+months[d.getMonth()]+ " " + 26 + ", " + d.getFullYear())
 
+// Dynamic Header 
 let theHeader = "Welcome to"
 
 document.getElementById("header").innerHTML = (theHeader)
 
+// Welcome Message 
 onload(alert("Welcome to hear.com"))
 
+// Pulling form data 
 let formData = new FormData(document.querySelector("form"))
